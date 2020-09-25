cd3d3bb1746f9dde1efc8ad4ae79238b4e2306e7
Roberto Villegas-Diaz <villegas.roberto@hotmail.com>
2020-09-25 10:27:43 +0100

    Adding tests for utilitary functions

---
+++ tests/testthat.R
@@ -0,0 +1,4 @@
+library(testthat)
+library(reconbio)
+
+test_check("reconbio")
---
+++ tests/testthat/test-utils.R
@@ -0,0 +1,22 @@
+test_that("hexagonal logo works", {
+  hex_logo(output = "hex_logo.png")
+  expect_true(file.exists("hex_logo.png"))
+  expect_false(dir.exists("hex_logo.png"))
+  expect_gt(file.size("hex_logo.png"), 0)
+  file.remove("hex_logo.png")
+  expect_false(file.exists("hex_logo.png"))
+})
+
+test_that("parallel benchmark works", {
+  # Define toy function that sleeps for (60/cpus) seconds
+  a <- function(cpus) {Sys.sleep(4/cpus)}
+  times_df <- par_benchmark(c(1, 2, 4), a, quiet = TRUE)
+  expect_equal(floor(times_df$times), c(4, 2, 1))
+  expect_output(par_benchmark(c(4), a, quiet = FALSE))
+  expect_output(par_benchmark(c(4), a, quiet = FALSE, plot = TRUE))
+  expect_true(file.exists("Rplots.pdf"))
+  expect_false(dir.exists("Rplots.pdf"))
+  expect_gt(file.size("Rplots.pdf"), 0)
+  file.remove("Rplots.pdf")
+  expect_false(file.exists("Rplots.pdf"))
+})
