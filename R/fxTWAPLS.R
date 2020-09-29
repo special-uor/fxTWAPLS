f8a0b06fbf0043a4992389db1f2edaa5e12ab923
Roberto Villegas-Diaz <villegas.roberto@hotmail.com>
2020-09-29 17:00:31 +0100

    Adding examples for cv.w

--- R/fxTWAPLS.R
+++ R/fxTWAPLS.R
@@ -651,7 +651,46 @@ sse.sample <- function(modern_taxa,
 #' @return leave-one-out cross validation results
 #' @export
 #'
-# @examples
+#' @examples
+#' \dontrun{
+#'     # Load modern pollen data
+#'     modern_pollen <- read.csv(system.file("extdata", 
+#'                                          "Modern_Pollen_gdd_alpha_Tmin.csv", 
+#'                                          package = "fxTWAPLS", 
+#'                                          mustWork = TRUE))
+#'     
+#'     # Extract taxa
+#'     taxaColMin <- which(colnames(modern_pollen) == "Abies")
+#'     taxaColMax <- which(colnames(modern_pollen) == "Zygophyllaceae")
+#'     taxa <- modern_pollen[, taxaColMin:taxaColMax]
+#'     
+#'     # Get the frequency of each climate variable fx
+#'     fx_Tmin <- fxTWAPLS::fx(modern_pollen$Tmin, bin = 0.02)
+#'     fx_gdd <- fxTWAPLS::fx(modern_pollen$gdd, bin = 20)
+#'     fx_alpha <- fxTWAPLS::fx(modern_pollen$alpha, bin = 0.002)
+#'     
+#'     # MTCO
+#'     ## fx
+#'     fit_Tmin <- fxTWAPLS::WAPLS.w(taxa, modern_pollen$Tmin, nPLS = 5)
+#'     
+#'     ## LOOCV
+#'     ### without fx
+#'     cv_Tmin <- fxTWAPLS::cv.w(taxa,
+#'                               modern_pollen$Tmin,
+#'                               nPLS = 5,
+#'                               fxTWAPLS::WAPLS.w,
+#'                               fxTWAPLS::WAPLS.predict.w,
+#'                               cpus = 1)
+#'     ### with fx
+#'     cv_f_Tmin <- fxTWAPLS::cv.w(taxa,
+#'                                 modern_pollen$Tmin,
+#'                                 nPLS = 5,
+#'                                 fxTWAPLS::WAPLS.w,
+#'                                 fxTWAPLS::WAPLS.predict.w,
+#'                                 usefx = TRUE,
+#'                                 fx = fx_Tmin,
+#'                                 cpus = CPUS)  
+#' }
 cv.w <- function(modern_taxa,
                  modern_climate,
                  nPLS = 5,
