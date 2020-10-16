b1e659041f71c9987511e3980ac8c56b5a167542
Roberto Villegas-Diaz <villegas.roberto@hotmail.com>
2020-10-16 09:31:29 +0100

    Limiting tests to only 2 CPUs

--- tests/testthat/test-utils.R
+++ tests/testthat/test-utils.R
@@ -9,11 +9,11 @@ test_that("hexagonal logo works", {
 
 test_that("parallel benchmark works", {
   # Define toy function that sleeps for (60/cpus) seconds
-  a <- function(cpus) {Sys.sleep(4/cpus)}
-  times_df <- par_benchmark(c(1, 2, 4), a, quiet = TRUE)
-  expect_equal(length(times_df$times), 3)
-  expect_output(par_benchmark(c(4), a, quiet = FALSE))
-  expect_output(par_benchmark(c(4), a, quiet = FALSE, plot = TRUE))
+  a <- function(cpus) {Sys.sleep(2/cpus)}
+  times_df <- par_benchmark(c(1, 2), a, quiet = TRUE)
+  expect_equal(length(times_df$times), 2)
+  expect_output(par_benchmark(c(2), a, quiet = FALSE))
+  expect_output(par_benchmark(c(2), a, quiet = FALSE, plot = TRUE))
   # print(list.files("."))
   # is.windows <- Sys.info()['sysname'] == "Windows"
   # # print(paste0("Is windows ", is.windows))
