29629aaa8e179286ba375feb223749ab3286d1b8
Roberto Villegas-Diaz <villegas.roberto@hotmail.com>
2021-08-31 20:44:05 +0100

    Add namespace prefix to fx and fx_pspline calls

--- R/fxTWAPLS.R
+++ R/fxTWAPLS.R
@@ -253,10 +253,10 @@ WAPLS.w <- function(modern_taxa,
   } else {
     if (fx_method == "bin"){
       lm <- MASS::rlm(modern_climate ~ comp[, 1:pls], 
-                      weights = 1 / fx(x,bin) ^ 2)
+                      weights = 1 / fxTWAPLS::fx(x, bin) ^ 2)
     } else {
       lm <- MASS::rlm(modern_climate ~ comp[, 1:pls], 
-                      weights = 1 / fx_pspline(x,bin) ^ 2)
+                      weights = 1 / fxTWAPLS::fx_pspline(x, bin) ^ 2)
     }
   }
   
@@ -309,10 +309,10 @@ WAPLS.w <- function(modern_taxa,
     } else {
       if (fx_method=="bin"){
         lm <- MASS::rlm(modern_climate ~ comp[, 1:pls], 
-                        weights = 1 / fx(x,bin) ^ 2)
+                        weights = 1 / fxTWAPLS::fx(x, bin) ^ 2)
       } else {
         lm <- MASS::rlm(modern_climate ~ comp[, 1:pls], 
-                        weights = 1 / fx_pspline(x,bin) ^ 2)
+                        weights = 1 / fxTWAPLS::fx_pspline(x, bin) ^ 2)
       }
     }
     
@@ -474,10 +474,10 @@ TWAPLS.w <- function(modern_taxa,
   } else {
     if (fx_method=="bin"){
       lm <- MASS::rlm(modern_climate ~ comp[, 1:pls], 
-                      weights = 1 / fx(x,bin) ^ 2)
+                      weights = 1 / fxTWAPLS::fx(x, bin) ^ 2)
     } else {
       lm <- MASS::rlm(modern_climate ~ comp[, 1:pls], 
-                      weights = 1 / fx_pspline(x,bin) ^ 2)
+                      weights = 1 / fxTWAPLS::fx_pspline(x, bin) ^ 2)
     }
   }
   
@@ -535,10 +535,10 @@ TWAPLS.w <- function(modern_taxa,
     } else {
       if (fx_method=="bin"){
         lm <- MASS::rlm(modern_climate ~ comp[, 1:pls], 
-                        weights = 1 / fx(x,bin) ^ 2)
+                        weights = 1 / fxTWAPLS::fx(x, bin) ^ 2)
       } else {
         lm <- MASS::rlm(modern_climate ~ comp[, 1:pls], 
-                        weights = 1 / fx_pspline(x,bin) ^ 2)
+                        weights = 1 / fxTWAPLS::fx_pspline(x, bin) ^ 2)
       }
     }
     
